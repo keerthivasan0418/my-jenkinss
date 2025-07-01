e4b6d57c3457bbde490d05ef79475613b884e7d0
keerthivasan0418 <keerthivasanbmbk@gmail.com>
2025-07-01 14:58:43 +0530

    syntax changed

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -4,5 +4,5 @@
 aws eks update-kubeconfig --region ap-south-1 --name trend-cluster
 
 # making the kubernetes manifest files work :)
-kubectl apply -f k8s/deployment.yml
+kubectl apply -f deployment.yml
 
