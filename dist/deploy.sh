d0c2184fdfb62f3d71d53e9f8cadd4354f679606
keerthivasan0418 <keerthivasanbmbk@gmail.com>
2025-07-01 14:46:20 +0530

    made some changes

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # connecting the cluster :)
-aws eks update-kubeconfig --region us-east-1 --name your-cluster-name
+aws eks update-kubeconfig --region ap-south-1 --name trend-cluster
 
 # making the kubernetes manifest files work :)
 kubectl apply -f k8s/deployment.yml
